f3818bb3a6a65763b237e14833df0ec332c6cbad
Tiago Emerick <tiagoeborsanyi@gmail.com>
2016-09-19 12:49:11 -0300

    incluida uma linha de conexao com o banco de dados do mlab

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var app = require('./config/express')();
 require('./config/passport')();
 require('./config/database.js')('mongodb://localhost/cartic');
+//require('./config/database.js')('mongodb://root:260584@ds045882.mlab.com:45882/cartic');
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express cartic escutando na porta '+app.get('port'));
