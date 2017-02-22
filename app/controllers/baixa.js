16dd91caaac2dea63c162227eb6b660bbeb0352e
Tiago Emerick <tiagoeborsanyi@gmail.com>
2017-02-22 15:43:52 -0300

    estrutura inicial do server feita para baixa de equipamentos

--- app/controllers/baixa.js
+++ app/controllers/baixa.js
@@ -1,3 +1,27 @@
 module.exports = function (app) {
 
+  var Baixa = app.models.baixa;
+	var Equipamento = app.models.equipamento;
+	var Lotacao = app.models.lot;
+
+  var controller = {};
+
+  controller.listaBaixas = function (req, res) {
+    res.json({data: 'dados de baixa'});
+  };
+
+  controller.obtemBaixa = function (req, res) {
+
+  };
+
+  controller.removeBaixa = function (req, res) {
+
+  };
+
+  controller.salvaBaixa = function (req, res) {
+
+  };
+
+  return controller;
+
 };
--- app/routes/baixa.js
+++ app/routes/baixa.js
@@ -1,5 +1,23 @@
+function verifica(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}else{
+		res.status('401').json('não autorizado');
+	}
+};
+
 module.exports = function (app) {
 
   var controller = app.controllers.baixa;
+
+  app.route('/baixa/equipamentos/')
+    .get(controller.listaBaixas)
+    .post(controller.salvaBaixa);
+
+  app.route('baixa/equipamento/:id')
+    .get(controller.obtemBaixa)
+    .delete(controller.removeBaixa);
+
+
 
 };
